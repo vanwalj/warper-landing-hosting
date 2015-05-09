09b069202c66ddb3137fdf11400605ae39614b9d
Jordan <j.vanwall@gmail.com>
2015-05-09 02:12:50 +0100

    Update google spreadsheet

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # warper-landing-hosting
+
+ENV
+-----------
+G_SPREADSHEET_ID
+G_USER_PASSWORD
+G_USER_LOGIN
+
+PORT
+DB_URI
--- index.js
+++ index.js
@@ -22,7 +22,6 @@ app.post('/newsletter', function (req, res, next) {
             sheet.setAuth(process.env.G_USER_LOGIN, process.env.G_USER_PASSWORD, function (err) {
                 if (err) return console.error(err);
                 sheet.addRow(1, {
-                    id: newsletter.id,
                     email: newsletter.email,
                     userAgent: req.headers['user-agent'],
                     date: new Date()
